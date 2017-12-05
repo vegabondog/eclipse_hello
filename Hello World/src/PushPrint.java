d56823ca350cbbc55fa629a926c1f4ca53a61d4b
vegabondog <vegabondog@outlook.kr>
2017-12-05 18:48:04 +0900

    PushPrint.java modified on github

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -4,7 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Add Java Class File Push");
-		//워킹트리에서 작업
+		System.out.print("World with 365");
 	}
 
 }
